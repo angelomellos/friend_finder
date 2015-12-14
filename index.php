dd26cb07fc049a9322d379839d0dde4041f59b84
Angelo <angelopmellos@gmail.com>
2015-12-14 14:06:53 -0500

    sessions

--- index.php
+++ index.php
@@ -3,6 +3,17 @@ require 'vendor/autoload.php';
 use Elasticsearch\ClientBuilder;
 $client = ClientBuilder::create()->build();
 
+if (!isset($_SESSION))
+  session_start();
+
+if (isset($_SESSION['auth_user']) && $_SESSION["auth_user"]) {
+  echo 'main page here';
+} else{
+  if($_SESSION['invalid'])
+    echo 'invalid username or password';
+  echo 'login form here';
+}
+
 $params['index'] = 'pokemon';
 $params['type'] = 'pokemon_trainer';
 $params['body']['query']['match']['age'] = 15;
