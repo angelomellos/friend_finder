816afe7fad8e7b9789ad13c778f4c7b3901b7cdc
Angelo <angelopmellos@gmail.com>
2015-12-14 11:37:12 -0500

    fixed typo

--- auth.php
+++ auth.php
@@ -2,14 +2,16 @@
 require 'vendor/autoload.php';
 $user = getenv('DBUSER');
 $pass = getenv('DBPASS');
-try {
-   $db = new PDO('mysql:host=localhost;dbname=friend_finder', $user, $pass);
-    $users = $db->query('SELECT * from users');
-    $db = null;
-   }
-} catch (PDOException $e) {
-   print "Error!: " . $e->getMessage() . "<br/>";
-    die();
+try
+{
+  $db = new PDO('mysql:host=localhost;dbname=friend_finder', $user, $pass);
+  $users = $db->query('SELECT * from users');
+  $db = null;
+}
+catch (PDOException $e)
+{
+  print "Error!: " . $e->getMessage() . "<br/>";
+  die();
 }
 print_r($users);
 
