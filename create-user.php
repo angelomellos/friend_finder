07148712e11799feb5c004ee5b0bc1743131a6ae
Angelo <angelopmellos@gmail.com>
2015-12-14 16:26:34 -0500

    add users into database

--- create-user.php
+++ create-user.php
@@ -1,3 +1,21 @@
 <?php
-
+require 'vendor/autoload.php';
+$config = parse_ini_file('/../../../config.ini');
+$dbuser = $config['username'];
+$dbpass = $config['password'];
+session_start();
+$email = htmlspecialchars($_POST["email"]);
+$password = htmlspecialchars($_POST["password"]);
+try
+{
+  $db = new PDO('mysql:host=localhost;dbname=friend_finder', $dbuser, $dbpass);
+  $query = $db->prepare("INSERT INTO users VALUES ($email, $password)");
+  $query->execute();
+  $db = null;
+}
+catch (PDOException $e)
+{
+  print "Error!: " . $e->getMessage() . "<br/>";
+  die();
+}
 ?>
