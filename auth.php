dc7f2f85dc52331595d04a70006a996fcef081c7
angelomellos <angelopmellos@gmail.com>
2015-12-14 13:52:48 -0500

    basic user auth

--- auth.php
+++ auth.php
@@ -1,11 +1,16 @@
 <?php
 require 'vendor/autoload.php';
-$user = getenv('DBUSER');
-$pass = getenv('DBPASS');
+$config = parse_ini_file('/../../../config.ini');
+$dbuser = $config['username'];
+$dbpass = $config['password'];
+$email = htmlspecialchars($_POST["email"]);
+$user_pass = htmlspecialchars($_POST["pass"]);
 try
 {
-  $db = new PDO('mysql:host=localhost;dbname=friend_finder', $user, $pass);
-  $users = $db->query('SELECT * from users');
+  $db = new PDO('mysql:host=localhost;dbname=friend_finder', $dbuser, $dbpass);
+  $query = $db->prepare("SELECT * from users WHERE email='$email'");
+  $query->execute();
+  $stored_pass = $query->fetchAll()[0]['password'];
   $db = null;
 }
 catch (PDOException $e)
@@ -13,6 +18,11 @@ catch (PDOException $e)
   print "Error!: " . $e->getMessage() . "<br/>";
   die();
 }
-print_r($users);
-
+if ($stored_pass == $user_pass){
+  _SESSION['auth_user']= TRUE;
+  header('Location: index.php');
+  exit();
+} else{
+  print 'invalid credentials';
+}
 ?>
