ed4b949979717b561f174efab3d72c506dae6715
Angelo <angelopmellos@gmail.com>
2015-12-14 14:34:02 -0500

    added basic login form

--- index.php
+++ index.php
@@ -17,17 +17,17 @@ if (isset($_SESSION['auth_user']) && $_SESSION["auth_user"]) {
   echo $m->render('login-form');
 }
 
-$params['index'] = 'pokemon';
-$params['type'] = 'pokemon_trainer';
-$params['body']['query']['match']['age'] = 15;
-try{
-  $result = $client->search($params);
-  print_r($result['hits']['hits'][0]['_source']['name']);
-  echo "<br>";
-  print_r($result['hits']['hits'][0]['_source']['age']);
-  echo "<br>";
-  print_r($result['hits']['hits'][0]['_source']['badges']);
-} catch(Exception $e) {
-  print_r($e);
-}
+// $params['index'] = 'pokemon';
+// $params['type'] = 'pokemon_trainer';
+// $params['body']['query']['match']['age'] = 15;
+// try{
+//   $result = $client->search($params);
+//   print_r($result['hits']['hits'][0]['_source']['name']);
+//   echo "<br>";
+//   print_r($result['hits']['hits'][0]['_source']['age']);
+//   echo "<br>";
+//   print_r($result['hits']['hits'][0]['_source']['badges']);
+// } catch(Exception $e) {
+//   print_r($e);
+// }
 ?>
